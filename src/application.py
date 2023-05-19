594cde3dae1452cb34bbffc18eb634da5674d9c2
Mattias <mattias@ankra.io>
2023-05-19 13:58:17 +0200

    base setup

---
+++ src/application.py
@@ -0,0 +1,38 @@
+from flask import Flask
+
+
+def create_app() -> Flask:
+    # Define Flask Application as app.
+    app = Flask(__name__)
+    
+    # Load Configurations
+    app.config['ENV'] = 'development'
+    app.config['DEBUG'] = False
+    app.config['TESTING'] = False
+
+    # Initialize Blueprints
+    initialize_blueprints(app)
+
+    # Log Application Mode.
+    print(
+        f"Application running in {app.config['ENV']} mode"
+    )
+    # Return and start Flask application.
+    return app
+
+
+def initialize_blueprints(app: object) -> None:
+    # Recipe Blueprints
+    from recipes import (
+        recipes_bp
+    )
+    app.register_blueprint(
+        recipes_bp
+    )
+    # System Blueprints
+    from system import (
+        system_bp
+    )
+    app.register_blueprint(
+        system_bp
+    )
